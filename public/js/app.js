19176ff51e22b3f08f67647e1a37a8fdc387bd51
Harell Juanico <harell286@gmail.com>
2020-08-08 01:20:44 +0800

    Integrated API with UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-console.log(213);
+const form = $('form');
+
+const button = form.find('button');
+const input = form.find('input');
+
+button.on('click', function(e) {
+	e.preventDefault();
+
+	if (input.val()) {
+		$(this).attr('disabled', true);
+
+		fetch('http://localhost:3000/weather?address='+input.val()).then((response) => {
+			
+			const p = $(this).siblings().closest('p');
+			
+			response.json().then(({ error = "", address = "", forecast = "", location = "" }) => {
+				if (error) {
+					p.text(error);
+				} else {
+					p.text(forecast);
+				}
+
+				$(this).attr('disabled', false);
+			})
+		});
+	}
+
+});
